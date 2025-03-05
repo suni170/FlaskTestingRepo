9bffa8d7cbbe4b8457a01d6ecea5d6d9ed989d78
Suni170 <suneetha2201@gmail.com>
2025-03-06 00:22:44 +0530

    updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "hello world"
+    return "hello world this is the testing new file"
 
 # Run the Flask app
 if __name__ == '__main__':
