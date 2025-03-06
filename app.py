46bf11ccf231b4fb029c1056e166e2a145c4c8c0
Suni170 <suneetha2201@gmail.com>
2025-03-06 08:55:52 +0530

    updated app.py file

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
+from child_package.child_script import main  # Import function correctly
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/parent')
 def hello():
     return "hello world this is the testing new file"
 
-# Run the Flask app
+@app.route('/')
+def child():
+    try:
+        output = main()  # Calling function from child repo
+        return jsonify({"message": "Child triggered", "output": output})
+    except Exception as e:
+        return jsonify({"error": "Error executing child script", "details": str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
